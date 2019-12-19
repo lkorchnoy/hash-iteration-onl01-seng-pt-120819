b5756d50725999a32aa0c5fadafd571238f74929
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-19 08:31:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,7 +9,11 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
-  happy_birthday()
+  happy_birthday({
+   "Timmy" => 9, 
+  "Sarah" => 6, 
+  "Amanda" => 27
+ })
 end
 
 
